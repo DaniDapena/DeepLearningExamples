e739ec8a94ec76cd4e23d81bed1416d8b652e1f9
DaniDapena <danidapena3093@gmail.com>
2023-08-22 00:51:02 +0000

    update

--- PyTorch/Classification/ConvNets/image_classification/optimizers.py
+++ PyTorch/Classification/ConvNets/image_classification/optimizers.py
@@ -5,29 +5,16 @@ import torch
 from torch import optim
 
 
-def get_optimizer(parameters, lr, args, state=None):
-    if args.optimizer == "sgd":
-        optimizer = get_sgd_optimizer(
-            parameters,
-            lr,
-            momentum=args.momentum,
-            weight_decay=args.weight_decay,
-            nesterov=args.nesterov,
-            bn_weight_decay=args.bn_weight_decay,
-        )
-    elif args.optimizer == "rmsprop":
-        optimizer = get_rmsprop_optimizer(
-            parameters,
-            lr,
-            alpha=args.rmsprop_alpha,
-            momentum=args.momentum,
-            weight_decay=args.weight_decay,
-            eps=args.rmsprop_eps,
-            bn_weight_decay=args.bn_weight_decay,
-        )
-    if not state is None:
-        optimizer.load_state_dict(state)
 
+
+def get_optimizer(parameters, lr, momentum,weight_decay,nesterov,state=None):
+    optimizer = get_sgd_optimizer(
+            parameters,
+            lr,
+            momentum=momentum,
+            weight_decay=weight_decay,
+            nesterov=nesterov,
+    )
     return optimizer
 
 
